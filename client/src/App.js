3a43dbd9eb7dcd92abcf2da279dd08fb7e7d06c8
Ali Rajaii <rajaii_a@yahoo.com>
2019-06-08 11:50:22 -0400

    MVP done!

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Route } from 'react-router-dom';
+import axios from 'axios';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
@@ -8,21 +9,29 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      savedList: []
+      savedList: [],
+      
     };
   }
+
+  
+
+  
 
   addToSavedList = movie => {
     const savedList = this.state.savedList;
     savedList.push(movie);
     this.setState({ savedList });
   };
+  
 
   render() {
+    
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path='/' render={(props) => <MovieList {...props} />}/>
+        <Route exact path={`/movies/:id`} render={(props) => <Movie {...props}  />}/>
       </div>
     );
   }
